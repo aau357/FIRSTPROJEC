5445f1352b3b6dcf210bf488f9c84a59a2383260
aau357 <ashurboyevalimardon@gmail.com>
2026-01-30 13:44:42 +0500

    Vazifa bajarildi teacher

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
+import { HeartCrack, Package, ShoppingCart, UserRoundPen } from 'lucide-react'
 import './App.css'
 function App() {
   return (
     //  main parrent
-    <div className='container'>
-     {/* card */}
-      <div className='flex border rounded-2xl p-1 items-center justify-center m-2 gap-1 bg-gray-500 lg:'>
-        <span className='text-sm'>
-          Biz o'rtog'im bilan o'z startup loyihamizni qurish maqsadida o'zimizni birinchi web-saytimizni qurdik.
+    <div className='container flex gap-4 justify-between mt-4'>
+      <div className='ms-4'>
+        <span className='text-4xl font-bold text-amber-500'>
+          Vip
         </span>
-       {/* image */}
-        <div className=''>
-          <img className='rounded-2xl w-50 h-17'
-          src="./futurestartup.jpg" 
-          alt="Rasm" />
-          <span className='text-xs italic text-gray-400'>Bizning logo</span>
+        <span className='text-blue-700 text-2xl'>
+          Plast
+        </span>
+      </div>
+
+      <div>
+        <input
+          type="search"
+          placeholder="Qidiruv"
+          className="border-2 border-gray-500 px-4 py-2 rounded-2xl outline-none w-250"
+
+        />
+      </div>
+      <div className='flex text-gray-700 gap-4 me-4 '>
+        <div className='flex flex-col items-center'>
+          <Package />
+          <span>Buyurtma xolati</span>
         </div>
+        <div className='flex flex-col items-center'>
+          <ShoppingCart />
+          <span>Savat</span>
+        </div>
+        <div className='flex flex-col items-center'>
+          <HeartCrack />
+          <span>Sevimli</span>
+        </div>
+        <div className='flex flex-col items-center'>
+          <UserRoundPen />
+          <span>Kirish</span>
+        </div>
+
       </div>
     </div>
   )
---
+++ src/assets/companents/card.tsx
@@ -0,0 +1,24 @@
+
+
+const card = () => {
+    return (
+        <div>
+            {/* card */}
+
+            < div className='flex border rounded-2xl p-1 items-center justify-center m-2 gap-1 bg-gray-500 lg:' >
+                <span className='text-sm'>
+                    Biz o'rtog'im bilan o'z startup loyihamizni qurish maqsadida o'zimizni birinchi web-saytimizni qurdik.
+                </span>
+            </div >
+            {/* image */}
+            < div className='' >
+                <img className='rounded-2xl w-50 h-17'
+                    src="./futurestartup.jpg"
+                    alt="Rasm" />
+                <span className='text-xs italic text-gray-400'>Bizning logo</span>
+            </div >
+        </div>
+    )
+}
+
+export default card
