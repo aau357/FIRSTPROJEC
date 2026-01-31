45536c3aef72e1a1433f7a1265096432d035b07a
aau357 <ashurboyevalimardon@gmail.com>
2026-01-31 10:16:52 +0500

    fix it

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,7 @@ import './App.css'
 function App() {
   return (
     <div>
-      <div className='flex border rounded-2xl flex-col w-40 justify-center items-center  gap-2 p-2'>
-        <button className='active:scale-95 bg-gray-600 w-20 hover:bg-gray-700 rounded-xl p-0.5 transition-all'>
-          primary
-        </button>
-        <button className='active:scale-95 bg-gray-600 w-20 hover:bg-gray-700 rounded-xl p-0.5 transition-all'>
-          secondary
-        </button>
-        <button className='active:scale-95 bg-gray-600 w-20 hover:bg-gray-700 rounded-xl p-0.5 transition-all'>
-          danger
-        </button>
-        <button className='active:scale-95 bg-gray-600 w-20 hover:bg-gray-700 rounded-xl p-0.5 transition-all'>
-          disabled
-        </button>
-      </div>
+      
     </div>
 
   )
