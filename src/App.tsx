b72e638368785fd0cbc1bfff6a007b0c79d6e012
aau357 <ashurboyevalimardon@gmail.com>
2026-02-20 20:43:40 +0500

    fix iyt

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-const App = () => {
-  return (
-    <div>
+import { useState } from "react";
+import type { ChangeEvent } from "react";
 
-    </div>
-  )
+interface PasswordField {
+  value: string;
+  show: boolean;
 }
 
-export default App
+function App() {
+  const [passwordField, setPasswordField] = useState<PasswordField>({
+    value: "",
+    show: false
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPasswordField((prev: PasswordField) => ({
+      ...prev,
+      value: e.target.value
+    }));
+  };
+
+  const togglePassword = () => {
+    setPasswordField((prev: PasswordField) => ({
+      ...prev,
+      show: !prev.show
+    }));
+  };
+
+  return (
+    <div>
+      <input
+        type={passwordField.show ? "text" : "password"}
+        value={passwordField.value}
+        onChange={handleChange}
+      />
+
+      <button type="button" onClick={togglePassword}>
+        {passwordField.show ? "Yashirish" : "Ko‘rsatish"}
+      </button>
+    </div>
+  );
+}
+
+export default App;
